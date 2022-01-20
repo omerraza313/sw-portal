f907b720ffb096971955197ae37d47dd37222eff
omerraza313 <omerraza313@gmail.com>
2022-01-19 21:16:43 -0800

    Routes and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BusinessController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SubCategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,6 +22,22 @@ Route::get('/', function () {
 });
 
 /**********Admin Dashboard Routes***********/
-Route::get('/admin', function () {
-    return view('admin.dashboard');
+
+//dashboard
+Route::group(['prefix'=>'admin'], function(){
+
+	Route::get('/', [AdminController::class, 'index'])->name('admin.dash');
+
+	//admin business
+	Route::get('/business', [BusinessController::class, 'index'])->name('admin.business');
+
+	//admin service
+	Route::get('/service', [ServiceController::class, 'index'])->name('admin.service');
+
+	//admin category
+	Route::get('/category', [CategoryController::class, 'index'])->name('admin.category');
+
+	//admin category
+	Route::get('/sub-category', [SubCategoryController::class, 'index'])->name('admin.sub-categpry');
+
 });
