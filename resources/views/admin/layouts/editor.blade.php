380cf61137e8c151b0635d1e7abf72002f341879
omerraza313 <omerraza313@gmail.com>
2022-02-04 21:14:05 -0800

    change editor master

--- resources/views/admin/layouts/editor.blade.php
+++ resources/views/admin/layouts/editor.blade.php
@@ -19,12 +19,7 @@
   <!-- SimpleMDE -->
   <link rel="stylesheet" href="{{ asset('backend_assets/plugins/simplemde/simplemde.min.css')}}">
 
-  <script type="text/javascript" src="https://code.jquery.com/jquery-1.9.1.min.js"></script> 
-<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />
-<script type="text/javascript" src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-<!-- include summernote css/js-->
-<link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.4/summernote.css" rel="stylesheet">
-<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.4/summernote.js"></script>
+
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
